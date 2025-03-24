a697fad74ba42704c0562b1fb9fe342826bda540
susmita-25 <pjambhale@targetrwe.com>
2025-03-24 22:52:34 +0530

    initial commit

---
+++ src/api-mocks/handlers/people.handler.ts
@@ -0,0 +1,14 @@
+import { http, delay, HttpResponse } from 'msw';
+
+import { API_RESOURCE } from "../../app/shared/constant";
+import { PEOPLE } from "../fixtures";
+import { delayedResponse } from "../utils";
+
+const BASE_URL = `/mock-api/${API_RESOURCE.PEOPLE}*`;
+
+export const getPeople = http.get(BASE_URL, () => {
+  delay(500);
+  return HttpResponse.json(PEOPLE);
+});
+
+export const handlers = [getPeople];
---
+++ src/api-mocks/utils.ts
@@ -0,0 +1,8 @@
+// import { context, createResponseComposition } from "msw";
+
+// export const delayedResponse = createResponseComposition(
+//   undefined,
+//   process.env.NODE_ENV !== "test" ? [context.delay(500)] : []
+// );
+
+export const NUMBER = 1;
